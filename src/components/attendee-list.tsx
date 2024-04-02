ae10d60ed5bc896a70b153df9ceac0330094a73b
rdg-404 <rodrigopaiva0502@gmail.com>
2024-04-01 23:33:19 -0300

    feat: create and style title and input search

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -1,3 +1,16 @@
+import { Search } from 'lucide-react'
+
 export function Attendee() {
-  return <h1>attendees</h1>
+  return (
+    <div className="flex gap-3 items-center">
+      <h1 className="text-2xl font-bold">Attendees</h1>
+      <div className="px-3 w-72 py-1.5 border border-white/10 rounded-lg text-sm flex items-center gap-3">
+        <Search className="size-4 text-emerald-300" />
+        <input
+          className="bg-transparent flex-1 outline-none"
+          placeholder="Search for attendees..."
+        />
+      </div>
+    </div>
+  )
 }
