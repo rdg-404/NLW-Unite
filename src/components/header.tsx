11909ce919ff78352cb9e9ec6d98d772945cdbdc
rdg-404 <rodrigopaiva0502@gmail.com>
2024-04-02 23:03:56 -0300

    create navlink component and extends props for HTML element a

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,13 @@
 import nlwUniteLogo from '../assets/nlw-unite-logo.svg'
+import { NavLink } from './nav-link'
 export function Header() {
   return (
     <div className="flex items-center gap-5 py-5">
       <img src={nlwUniteLogo} alt="" />
 
       <nav className="flex items-center gap-5">
-        <a href="" className="font-medium text-sm text-zinc-300">
-          Events
-        </a>
-        <a href="" className="font-medium text-sm">
-          Attendees
-        </a>
+        <NavLink href="/events">Events</NavLink>
+        <NavLink href="/attendees">Attendees</NavLink>
       </nav>
     </div>
   )
