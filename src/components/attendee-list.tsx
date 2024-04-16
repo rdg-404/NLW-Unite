9904eee1dd6f89de5e7f20fe48951f8b0eb73008
rdg-404 <rodrigopaiva0502@gmail.com>
2024-04-15 23:27:58 -0300

    evitando que recaregue toda a pagina ao reiniciar

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -27,7 +27,8 @@ interface Attendee {
 
 export function Attendee() {
   const [search, setSearch] = useState('')
-  const [page, setPage] = useState(1)
+  // const [page, setPage] = useState(1)
+  const page = 1
   const [total, setTotal] = useState(0)
   const [attendees, setAttendees] = useState<Attendee[]>([])
 
@@ -54,23 +55,26 @@ export function Attendee() {
 
   function onSearchInputChanged(event: ChangeEvent<HTMLInputElement>) {
     setSearch(event.target.value)
-    setPage(1)
+    // setPage(1)
   }
 
   function goToFirstPage() {
-    setPage(1)
+    // setPage(1)
   }
 
   function goToNextPage() {
-    setPage(page + 1)
+    // setPage(page + 1)
+    const url = new URL(window.location.toString())
+    url.searchParams.set('page', String(page + 1))
+    window.history.pushState({}, '', url)
   }
 
   function goToPreviousPage() {
-    setPage(page - 1)
+    // setPage(page - 1)
   }
 
   function goToLastPage() {
-    setPage(totalPages)
+    // setPage(totalPages)
   }
   return (
     <div className="flex flex-col gap-4">
